e32816086a2798f17575031a6606ec3f63884fe7
Akero <rish1994@gmail.com>
2023-08-23 12:09:35 +0530

    afds

--- app/src/main/java/com/acme/acmevendor/activity/dashboard/AddSiteDetailActivity.java
+++ app/src/main/java/com/acme/acmevendor/activity/dashboard/AddSiteDetailActivity.java
@@ -140,6 +140,7 @@ public class AddSiteDetailActivity extends AppCompatActivity implements Location
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult();
         if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED
