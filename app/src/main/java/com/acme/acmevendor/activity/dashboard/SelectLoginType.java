a1ea2dd386c10e1a2c15e80a03e7151dc0aa0696
Akero <rish1994@gmail.com>
2023-10-12 10:59:10 +0530

    Adfg

---
+++ app/src/main/java/com/acme/acmevendor/activity/dashboard/SelectLoginType.java
@@ -0,0 +1,16 @@
+package com.acme.acmevendor.activity.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.acme.acmevendor.R;
+
+public class SelectLoginType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_login_type);
+    }
+}
