2a94fd8b15c58dc20e0d65e2df3cb10c0e9a55a3
pifon <rish1994@gmail.com>
2023-10-30 15:25:15 +0530

    reverted file

---
+++ app/src/main/java/com/acme/acmevendor/activity/dashboard/ViewVendorSites.java
@@ -0,0 +1,16 @@
+package com.acme.acmevendor.activity.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.acme.acmevendor.R;
+
+public class ViewVendorSites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_vendor_sites);
+    }
+}
