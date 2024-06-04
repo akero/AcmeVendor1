6233af2498bd6a8d4134906414ddaa69f700e30e
akero <rish1994@gmail.com>
2024-06-04 17:40:40 +0530

    dsadsa

--- app/src/main/java/com/acme/acmevendor/activity/dashboard/RecceHistoryDetails.java
+++ app/src/main/java/com/acme/acmevendor/activity/dashboard/RecceHistoryDetails.java
@@ -1,24 +1,76 @@
 package com.acme.acmevendor.activity.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.acme.acmevendor.R;
+import com.acme.acmevendor.viewmodel.APIreferenceclass;
+import com.acme.acmevendor.viewmodel.ApiInterface;
+import com.acme.acmevendor.databinding.ActivityRecceHistoryDetailsBinding;
 
-public class RecceHistoryDetails extends AppCompatActivity {
+public class RecceHistoryDetails extends AppCompatActivity implements ApiInterface {
+    int id;
+    String logintoken;
+    String projectId;
+
+    ActivityRecceHistoryDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_recce_history_details);
+        //setContentView(R.layout.activity_recce_history_details);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recce_history_details);
+
+
+        id= 0;
+        logintoken= "";
+        projectId= "";
+
+        try{
+
+            id= getIntent().getIntExtra("id", 0);
+            logintoken= getIntent().getStringExtra("logintoken");
+            projectId= getIntent().getStringExtra("projectid");
+
+        }catch(Exception e){
+            Log.d("asdsad", e.toString());
+        }
+
+        binding.btnDownload.setOnClickListener(new View.OnClickListener() {
+                                                   @Override
+                                                   public void onClick(View view) {
+
+
+                                                   }
+                                               });
+
+
+        APIreferenceclass api= new APIreferenceclass(logintoken, this, id, projectId);
+
+    }
+
+    void implementUI(String response){
 
 
 
     }
+
+    @Override
+    public void onResponseReceived(String response){
+
+        implementUI(response);
+
+    }
+
+
 }
--- app/src/main/java/com/acme/acmevendor/viewmodel/APIreferenceclass.java
+++ app/src/main/java/com/acme/acmevendor/viewmodel/APIreferenceclass.java
@@ -178,6 +178,28 @@ public class APIreferenceclass {
 
     }
 
+    //recce history details
+    public APIreferenceclass(String logintoken, Context context, int id, String projectid){
+
+        //TODO change url
+
+        String url= "https://acme.warburttons.com/api/supervisor-project/"+ projectid;
+        querytype= 0;
+
+        Log.d("tg01", "a");
+
+        String jsonPayload= "{\"Authorization\":\"" +logintoken+"\"}";
+
+        Map<String, String> headers= new HashMap<>();
+        headers.put("Authorization", "Bearer "+logintoken);
+        headers.put("Content-Type", "application/json");
+
+        Log.d("cldbatest","jsonpayload is "+ jsonPayload);
+
+        callapi(headers, jsonPayload, context, querytype, url);
+
+    }
+
 
     //to fetch client's campaigns in clientdashfirstpage
     public APIreferenceclass(int clientId, int vendorclientorcampaign, String logintoken, Context context) {
